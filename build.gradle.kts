3fb550e399d9380c9b7e49a1f7427ff7331df9a4
Andrei Mukamolau <amv@bamboogroup.eu>
2023-09-22 13:26:34 +0300

    Cleanup

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,4 @@
-import org.jetbrains.compose.compose
 import org.jetbrains.compose.desktop.application.dsl.TargetFormat
-import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     kotlin("jvm") version "1.9.10"
